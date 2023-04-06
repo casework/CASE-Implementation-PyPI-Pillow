ae1dab21b45b23a1f0e95de4569d261d7c871fea
red-ship-it <dowdallr@ucd.ie>
2023-04-06 04:08:48 +0100

    Updating actions to use same format as other CASE repositories

--- pillow_case/exif_node_maker.py
+++ pillow_case/exif_node_maker.py
@@ -3,17 +3,24 @@
 
 """Pillow to Case rdflib node maker"""
 
-import os
-import rdflib
 import logging
+import os
+
+import rdflib
 
 NS_RDF = rdflib.RDF
 NS_RDFS = rdflib.RDFS
 NS_UCO_CORE = rdflib.Namespace("https://unifiedcyberontology.org/ontology/uco/core#")
-NS_UCO_LOCATION = rdflib.Namespace("https://unifiedcyberontology.org/ontology/uco/location#")
-NS_UCO_OBSERVABLE = rdflib.Namespace("https://unifiedcyberontology.org/ontology/uco/observable#")
+NS_UCO_LOCATION = rdflib.Namespace(
+    "https://unifiedcyberontology.org/ontology/uco/location#"
+)
+NS_UCO_OBSERVABLE = rdflib.Namespace(
+    "https://unifiedcyberontology.org/ontology/uco/observable#"
+)
 NS_UCO_TYPES = rdflib.Namespace("https://unifiedcyberontology.org/ontology/uco/types#")
-NS_UCO_VOCABULARY = rdflib.Namespace("https://unifiedcyberontology.org/ontology/uco/vocabulary#")
+NS_UCO_VOCABULARY = rdflib.Namespace(
+    "https://unifiedcyberontology.org/ontology/uco/vocabulary#"
+)
 NS_XSD = rdflib.namespace.XSD
 
 _logger = logging.getLogger(os.path.basename(__file__))
@@ -31,31 +38,11 @@ def n_cyber_object_to_node(graph):
     n_file_facets = rdflib.BNode()
     n_content_facets = rdflib.BNode()
     n_exif_facets = rdflib.BNode()
-    graph.add((
-        cyber_object_facet,
-        NS_RDF.type,
-        NS_UCO_OBSERVABLE.ObservableObject
-    ))
-    graph.add((
-        cyber_object_facet,
-        NS_UCO_OBSERVABLE.hasChanged,
-        rdflib.Literal(False)
-    ))
-    graph.add((
-        cyber_object_facet,
-        NS_UCO_CORE.hasFacet,
-        n_exif_facets
-    ))
-    graph.add((
-        cyber_object_facet,
-        NS_UCO_CORE.hasFacet,
-        n_raster_facets
-    ))
-    graph.add((
-        cyber_object_facet,
-        NS_UCO_CORE.hasFacet,
-        n_file_facets
-    ))
+    graph.add((cyber_object_facet, NS_RDF.type, NS_UCO_OBSERVABLE.ObservableObject))
+    graph.add((cyber_object_facet, NS_UCO_OBSERVABLE.hasChanged, rdflib.Literal(False)))
+    graph.add((cyber_object_facet, NS_UCO_CORE.hasFacet, n_exif_facets))
+    graph.add((cyber_object_facet, NS_UCO_CORE.hasFacet, n_raster_facets))
+    graph.add((cyber_object_facet, NS_UCO_CORE.hasFacet, n_file_facets))
     return n_exif_facets, n_raster_facets, n_file_facets, n_content_facets
 
 
@@ -67,35 +54,32 @@ def filefacets_object_to_node(graph, n_file_facets, file_information):
     :param file_information: Dictionary containing information about file being analysed
     :return: None
     """
-    file_name, ext = os.path.splitext(file_information['Filename'])
+    file_name, ext = os.path.splitext(file_information["Filename"])
     file_ext = ext[1:]
-    graph.add((
-        n_file_facets,
-        NS_RDF.type,
-        NS_UCO_OBSERVABLE.FileFacet
-    ))
-    graph.add((
-        n_file_facets,
-        NS_UCO_OBSERVABLE.fileName,
-        rdflib.Literal(os.path.basename(file_information["Filename"]))
-    ))
-    graph.add((
-        n_file_facets,
-        NS_UCO_OBSERVABLE.filePath,
-        rdflib.Literal(os.path.abspath(file_information["Filename"]))
-    ))
-    graph.add((
-        n_file_facets,
-        NS_UCO_OBSERVABLE.extension,
-        rdflib.Literal(file_ext)
-    ))
-    if 'size' in file_information.keys():
-        graph.add((
+    graph.add((n_file_facets, NS_RDF.type, NS_UCO_OBSERVABLE.FileFacet))
+    graph.add(
+        (
             n_file_facets,
-            NS_UCO_OBSERVABLE.sizeInBytes,
-            rdflib.term.Literal(file_information["size"],
-                                datatype=NS_XSD.integer)
-        ))
+            NS_UCO_OBSERVABLE.fileName,
+            rdflib.Literal(os.path.basename(file_information["Filename"])),
+        )
+    )
+    graph.add(
+        (
+            n_file_facets,
+            NS_UCO_OBSERVABLE.filePath,
+            rdflib.Literal(os.path.abspath(file_information["Filename"])),
+        )
+    )
+    graph.add((n_file_facets, NS_UCO_OBSERVABLE.extension, rdflib.Literal(file_ext)))
+    if "size" in file_information.keys():
+        graph.add(
+            (
+                n_file_facets,
+                NS_UCO_OBSERVABLE.sizeInBytes,
+                rdflib.term.Literal(file_information["size"], datatype=NS_XSD.integer),
+            )
+        )
 
 
 def filecontent_object_to_node(graph, n_content_facets, file_information):
@@ -108,49 +92,34 @@ def filecontent_object_to_node(graph, n_content_facets, file_information):
     """
     byte_order_facet = rdflib.BNode()
     file_hash_facet = rdflib.BNode()
-    graph.add((
-        n_content_facets,
-        NS_RDF.type,
-        NS_UCO_OBSERVABLE.ContentDataFacet
-    ))
-    graph.add((
-        n_content_facets,
-        NS_UCO_OBSERVABLE.byteOrder,
-        byte_order_facet
-    ))
-    graph.add((
-        byte_order_facet,
-        NS_RDF.type,
-        NS_UCO_VOCABULARY.EndiannessTypeVocab,
-    ))
-    graph.add((
-        byte_order_facet,
-        NS_UCO_VOCABULARY.value,
-        rdflib.Literal("Big-endian")
-    ))
-    if 'mimetype' in file_information.keys():
-        graph.add((
-            n_content_facets,
-            NS_UCO_OBSERVABLE.mimeType,
-            rdflib.Literal(file_information["mimetype"])
-        ))
-    if 'size' in file_information.keys():
-        graph.add((
-            n_content_facets,
-            NS_UCO_OBSERVABLE.sizeInBytes,
-            rdflib.term.Literal(file_information["size"],
-                                datatype=NS_XSD.integer)
-        ))
-    graph.add((
-        n_content_facets,
-        NS_UCO_OBSERVABLE.hash,
-        file_hash_facet
-    ))
-    graph.add((
-        file_hash_facet,
-        NS_RDF.type,
-        NS_UCO_TYPES.Hash
-    ))
+    graph.add((n_content_facets, NS_RDF.type, NS_UCO_OBSERVABLE.ContentDataFacet))
+    graph.add((n_content_facets, NS_UCO_OBSERVABLE.byteOrder, byte_order_facet))
+    graph.add(
+        (
+            byte_order_facet,
+            NS_RDF.type,
+            NS_UCO_VOCABULARY.EndiannessTypeVocab,
+        )
+    )
+    graph.add((byte_order_facet, NS_UCO_VOCABULARY.value, rdflib.Literal("Big-endian")))
+    if "mimetype" in file_information.keys():
+        graph.add(
+            (
+                n_content_facets,
+                NS_UCO_OBSERVABLE.mimeType,
+                rdflib.Literal(file_information["mimetype"]),
+            )
+        )
+    if "size" in file_information.keys():
+        graph.add(
+            (
+                n_content_facets,
+                NS_UCO_OBSERVABLE.sizeInBytes,
+                rdflib.term.Literal(file_information["size"], datatype=NS_XSD.integer),
+            )
+        )
+    graph.add((n_content_facets, NS_UCO_OBSERVABLE.hash, file_hash_facet))
+    graph.add((file_hash_facet, NS_RDF.type, NS_UCO_TYPES.Hash))
 
 
 def raster_object_to_node(graph, controlled_dict, n_raster_facets, file_information):
@@ -162,67 +131,89 @@ def raster_object_to_node(graph, controlled_dict, n_raster_facets, file_informat
     :param file_information: Dictionary containing information about file being analysed
     :return: None
     """
-    file_name, ext = os.path.splitext(file_information['Filename'])
+    file_name, ext = os.path.splitext(file_information["Filename"])
     file_ext = ext[1:]
-    graph.add((
-        n_raster_facets,
-        NS_RDF.type,
-        NS_UCO_OBSERVABLE.RasterPictureFacet
-    ))
-    graph.add((
-        n_raster_facets,
-        NS_UCO_OBSERVABLE.pictureType,
-        rdflib.Literal(file_ext)
-    ))
+    graph.add((n_raster_facets, NS_RDF.type, NS_UCO_OBSERVABLE.RasterPictureFacet))
+    graph.add(
+        (n_raster_facets, NS_UCO_OBSERVABLE.pictureType, rdflib.Literal(file_ext))
+    )
     # :TODO The below feels a bit hacky probably a better way to do it
-    if 'EXIF ExifImageLength' in controlled_dict.keys():
-        graph.add((
+    if "EXIF ExifImageLength" in controlled_dict.keys():
+        graph.add(
+            (
+                n_raster_facets,
+                NS_UCO_OBSERVABLE.pictureHeight,
+                rdflib.term.Literal(
+                    int(float(str(controlled_dict["EXIF ExifImageLength"]))),
+                    datatype=NS_XSD.integer,
+                ),
+            )
+        )
+    elif "ExifImageHeight" in controlled_dict.keys():
+        graph.add(
+            (
+                n_raster_facets,
+                NS_UCO_OBSERVABLE.pictureHeight,
+                rdflib.term.Literal(
+                    int(float(str(controlled_dict["ExifImageHeight"]))),
+                    datatype=NS_XSD.integer,
+                ),
+            )
+        )
+    if "EXIF ExifImageWidth" in controlled_dict.keys():
+        graph.add(
+            (
+                n_raster_facets,
+                NS_UCO_OBSERVABLE.pictureWidth,
+                rdflib.term.Literal(
+                    int(float(str(controlled_dict["EXIF ExifImageWidth"]))),
+                    datatype=NS_XSD.integer,
+                ),
+            )
+        )
+    elif "ExifImageWidth" in controlled_dict.keys():
+        graph.add(
+            (
+                n_raster_facets,
+                NS_UCO_OBSERVABLE.pictureWidth,
+                rdflib.term.Literal(
+                    int(float(str(controlled_dict["ExifImageWidth"]))),
+                    datatype=NS_XSD.integer,
+                ),
+            )
+        )
+    if "EXIF CompressedBitsPerPixel" in controlled_dict.keys():
+        graph.add(
+            (
+                n_raster_facets,
+                NS_UCO_OBSERVABLE.bitsPerPixel,
+                rdflib.term.Literal(
+                    int(float(str(controlled_dict["EXIF CompressedBitsPerPixel"]))),
+                    datatype=NS_XSD.integer,
+                ),
+            )
+        )
+    elif "CompressedBitsPerPixel" in controlled_dict.keys():
+        graph.add(
+            (
+                n_raster_facets,
+                NS_UCO_OBSERVABLE.bitsPerPixel,
+                rdflib.term.Literal(
+                    int(float(str(controlled_dict["CompressedBitsPerPixel"]))),
+                    datatype=NS_XSD.integer,
+                ),
+            )
+        )
+    graph.add(
+        (
             n_raster_facets,
-            NS_UCO_OBSERVABLE.pictureHeight,
-            rdflib.term.Literal(int(float(str(controlled_dict['EXIF ExifImageLength']))),
-                                datatype=NS_XSD.integer)
-        ))
-    elif 'ExifImageHeight' in controlled_dict.keys():
-        graph.add((
-            n_raster_facets,
-            NS_UCO_OBSERVABLE.pictureHeight,
-            rdflib.term.Literal(int(float(str(controlled_dict['ExifImageHeight']))),
-                                datatype=NS_XSD.integer)
-        ))
-    if 'EXIF ExifImageWidth' in controlled_dict.keys():
-        graph.add((
-            n_raster_facets,
-            NS_UCO_OBSERVABLE.pictureWidth,
-            rdflib.term.Literal(int(float(str(controlled_dict['EXIF ExifImageWidth']))),
-                                datatype=NS_XSD.integer)
-        ))
-    elif 'ExifImageWidth' in controlled_dict.keys():
-        graph.add((
-            n_raster_facets,
-            NS_UCO_OBSERVABLE.pictureWidth,
-            rdflib.term.Literal(int(float(str(controlled_dict['ExifImageWidth']))),
-                                datatype=NS_XSD.integer)
-        ))
-    if 'EXIF CompressedBitsPerPixel' in controlled_dict.keys():
-        graph.add((
-            n_raster_facets,
-            NS_UCO_OBSERVABLE.bitsPerPixel,
-            rdflib.term.Literal(int(float(str(controlled_dict['EXIF CompressedBitsPerPixel']))),
-                                datatype=NS_XSD.integer)
-        ))
-    elif 'CompressedBitsPerPixel' in controlled_dict.keys():
-        graph.add((
-            n_raster_facets,
-            NS_UCO_OBSERVABLE.bitsPerPixel,
-            rdflib.term.Literal(int(float(str(controlled_dict['CompressedBitsPerPixel']))),
-                                datatype=NS_XSD.integer)
-        ))
-    graph.add((
-        n_raster_facets,
-        NS_RDFS.comment,
-        rdflib.Literal("Information represented here from exif information not from "
-                       "file system stats except for extension, which uses os python lib")
-    ))
+            NS_RDFS.comment,
+            rdflib.Literal(
+                "Information represented here from exif information not from "
+                "file system stats except for extension, which uses os python lib"
+            ),
+        )
+    )
 
 
 def controlled_dictionary_object_to_node(graph, controlled_dict, n_exif_facet):
@@ -234,21 +225,9 @@ def controlled_dictionary_object_to_node(graph, controlled_dict, n_exif_facet):
     :return: None
     """
     n_controlled_dictionary = rdflib.BNode()
-    graph.add((
-        n_exif_facet,
-        NS_RDF.type,
-        NS_UCO_OBSERVABLE.EXIFFacet
-    ))
-    graph.add((
-        n_exif_facet,
-        NS_UCO_OBSERVABLE.exifData,
-        n_controlled_dictionary
-    ))
-    graph.add((
-        n_controlled_dictionary,
-        NS_RDF.type,
-        NS_UCO_TYPES.ControlledDictionary
-    ))
+    graph.add((n_exif_facet, NS_RDF.type, NS_UCO_OBSERVABLE.EXIFFacet))
+    graph.add((n_exif_facet, NS_UCO_OBSERVABLE.exifData, n_controlled_dictionary))
+    graph.add((n_controlled_dictionary, NS_RDF.type, NS_UCO_TYPES.ControlledDictionary))
     for key in sorted(controlled_dict.keys()):
         # :TODO stringing all values to ensure they are strings, open to input
         #  here as maybe assertion or a better way to do this
@@ -260,30 +239,13 @@ def controlled_dictionary_object_to_node(graph, controlled_dict, n_exif_facet):
             _logger.info("v_value = %r." % v_value)
             raise
         n_entry = rdflib.BNode()
-        graph.add((
-            n_controlled_dictionary,
-            NS_UCO_TYPES.entry,
-            n_entry
-        ))
-        graph.add((
-            n_entry,
-            NS_RDF.type,
-            NS_UCO_TYPES.ControlledDictionaryEntry
-        ))
-        graph.add((
-            n_entry,
-            NS_UCO_TYPES.key,
-            rdflib.Literal(key)
-        ))
-        graph.add((
-            n_entry,
-            NS_UCO_TYPES.value,
-            v_value
-        ))
+        graph.add((n_controlled_dictionary, NS_UCO_TYPES.entry, n_entry))
+        graph.add((n_entry, NS_RDF.type, NS_UCO_TYPES.ControlledDictionaryEntry))
+        graph.add((n_entry, NS_UCO_TYPES.key, rdflib.Literal(key)))
+        graph.add((n_entry, NS_UCO_TYPES.value, v_value))
 
 
 def init(tag_dict, file_info):
-
     out_graph = rdflib.Graph()
     out_graph.namespace_manager.bind("uco-core", NS_UCO_CORE)
     out_graph.namespace_manager.bind("uco-location", NS_UCO_LOCATION)
@@ -291,8 +253,12 @@ def init(tag_dict, file_info):
     out_graph.namespace_manager.bind("uco-types", NS_UCO_TYPES)
     out_graph.namespace_manager.bind("uco-vocabulary", NS_UCO_VOCABULARY)
 
-    exif_facet_node, raster_facets_node, file_facets_node, content_facets = \
-        n_cyber_object_to_node(out_graph)
+    (
+        exif_facet_node,
+        raster_facets_node,
+        file_facets_node,
+        content_facets,
+    ) = n_cyber_object_to_node(out_graph)
     controlled_dictionary_object_to_node(out_graph, tag_dict, exif_facet_node)
     filefacets_object_to_node(out_graph, file_facets_node, file_info)
     raster_object_to_node(out_graph, tag_dict, raster_facets_node, file_info)
--- pillow_case/pillow_case.py
+++ pillow_case/pillow_case.py
@@ -3,11 +3,12 @@
 
 """Pillow to Case implementation"""
 
-import os
+import argparse
+import hashlib
 import json
 import logging
-import hashlib
-import argparse
+import os
+
 import exif_node_maker as node_maker
 from PIL import Image
 from PIL.ExifTags import TAGS
@@ -25,7 +26,7 @@ _logger.setLevel(level=logging.DEBUG)
 
 def get_file_info(filepath):
     """
-    A function to get some basic information about the file application being run against
+    A function to get information about the file application being run
     :param filepath: The relative path to the image
     :return: A Dictionary with some information about the file
     """
@@ -33,9 +34,9 @@ def get_file_info(filepath):
     try:
         sha256 = hashlib.sha256(open(filepath, "rb").read())
         file_stats = os.stat(filepath)
-        file_information['Filename'] = filepath
-        file_information['size'] = file_stats.st_size
-        file_information['SHA256'] = sha256.hexdigest()
+        file_information["Filename"] = filepath
+        file_information["size"] = file_stats.st_size
+        file_information["SHA256"] = sha256.hexdigest()
     except IOError as io_e:
         print(io_e)
     except ValueError as v_e:
@@ -46,12 +47,12 @@ def get_file_info(filepath):
 def get_exif_with_pillow(filename):
     image = Image.open(filename)
     image.verify()
-    return image._getexif()
+    return image.getexif()
 
 
 def get_labeled_exif_from_pillow(raw_exif):
     labeled = {}
-    for (key, val) in raw_exif.items():
+    for key, val in raw_exif.items():
         labeled[TAGS.get(key)] = val
     return labeled
 
@@ -66,24 +67,28 @@ def main():
     file_info = get_file_info(local_file)
     tags = get_exif_with_pillow(local_file)
     tag_dict = get_labeled_exif_from_pillow(tags)
-    _logger.warning(f"Number of exif tags found in image '{local_file}' = {len(tag_dict)}")
+    _logger.warning(
+        f"Number of exif tags found in image '{local_file}' = {len(tag_dict)}"
+    )
 
     node_graph = node_maker.init(tag_dict, file_info)
 
-    context = {"kb": "http://example.org/kb/",
-               "rdf": "http://www.w3.org/1999/02/22-rdf-syntax-ns#",
-               "rdfs": "http://www.w3.org/2000/01/rdf-schema#",
-               "uco-core": "https://unifiedcyberontology.org/ontology/uco/core#",
-               "uco-location": "https://unifiedcyberontology.org/ontology/uco/location#",
-               "uco-observable": "https://unifiedcyberontology.org/ontology/uco/observable#",
-               "uco-types": "https://unifiedcyberontology.org/ontology/uco/types#",
-               "xsd": "http://www.w3.org/2001/XMLSchema#"}
+    context = {
+        "kb": "http://example.org/kb/",
+        "rdf": "http://www.w3.org/1999/02/22-rdf-syntax-ns#",
+        "rdfs": "http://www.w3.org/2000/01/rdf-schema#",
+        "uco-core": "https://unifiedcyberontology.org/ontology/uco/core#",
+        "uco-location": "https://unifiedcyberontology.org/ontology/uco/location#",
+        "uco-observable": "https://unifiedcyberontology.org/ontology/uco/observable#",
+        "uco-types": "https://unifiedcyberontology.org/ontology/uco/types#",
+        "xsd": "http://www.w3.org/2001/XMLSchema#",
+    }
 
-    graphed = node_graph.serialize(format='json-ld', context=context)
+    graphed = node_graph.serialize(format="json-ld", context=context)
 
     graph = json.dumps(graphed, indent=4)
-    case_json = json.loads(graph.encode('utf-8'))
-    with open(args.outfile, 'w') as file:
+    case_json = json.loads(graph.encode("utf-8"))
+    with open(args.outfile, "w") as file:
         file.write(case_json)
 
 
